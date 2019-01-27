eb08460a9078044f04078ef9d170d1f58a3a1071
Neproify <yamasaki3214@gmail.com>
2019-01-27 01:22:21 +0100

    Dodaj pliki projektów.

---
+++ windowsdeletefilesgenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace windowsdeletefilesgenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
